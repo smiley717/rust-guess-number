9971f2afc34d56ebfe977626f54fb7a13618c4f1
smiley717 <smiley.dev717@gmail.com>
2022-04-28 13:06:42 -0700

    Random guess numbers

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,13 @@
 use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
 
 fn main() {
     println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1..100);
+
+    println!("The secret number is {}", secret_number);
 
     println!("Please input your guess.");
 
@@ -10,5 +16,13 @@ fn main() {
         .read_line(&mut guess)
         .expect("Failed to read line");
 
+    let guess: u32 = guess.trim().parse().expect("Please type");
+
     println!("You guessed: {}", guess);
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("You win!"),
+    }
 }
